748e767c7aadd7b6ce88470151f2ce081f629006
CSE-17-dipshikha <dipshikhaarya157@gmail.com>
2026-05-31 22:09:00 +0530

    Add files via upload

---
+++ EmployeeController.java
@@ -0,0 +1,43 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.service.EmployeeService;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService service;
+
+    @PostMapping
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return service.saveEmployee(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return service.getAllEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployee(@PathVariable Long id) {
+        return service.getEmployeeById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable Long id,
+                                   @RequestBody Employee employee) {
+        return service.updateEmployee(id, employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteEmployee(@PathVariable Long id) {
+        service.deleteEmployee(id);
+        return "Employee Deleted Successfully";
+    }
+}
